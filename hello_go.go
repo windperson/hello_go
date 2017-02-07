ed5edcc2e3e7e5979c7f71b07a4e6c3ff0c84d41
Charles Pao <windperson@gmail.com>
2017-02-07 20:07:50 +0800

    type conversion

--- hello_go.go
+++ hello_go.go
@@ -6,7 +6,7 @@ func main() {
 	var aVar int32 = 15
 	fmt.Println("Address of a", &aVar)
 	aVar += 1
-	b := aVar
+	b := int(aVar)
 	fmt.Println("new value of a=", aVar, "and address of a=", &aVar)
 	fmt.Println("new value of b=", b, "and address of a=", &b)
 }
