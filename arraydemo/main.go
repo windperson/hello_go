384e4da0771849fee66d0e5b79f1a3a156e2149e
Charles Pao <windperson@gmail.com>
2017-02-14 20:00:18 +0800

    the sampe operation using slice

--- arraydemo/main.go
+++ arraydemo/main.go
@@ -2,23 +2,16 @@ package main
 
 import "fmt"
 
-const gCapicity int = 5
-
-var g_items [gCapicity]string
-var g_length = 0
+var g_items []string
 
 func addItem(a string) {
-	if g_length < gCapicity {
-		g_items[g_length] = a
-		g_length++
-	} else {
-		fmt.Println("Already full!")
-	}
+	fmt.Println("g_items current Capacity",cap(g_items))
+    g_items = append(g_items,a)
 }
 
 func printItems() {
 	fmt.Println("g_items content:")
-	for i := 0; i < g_length; i++ {
+	for i := 0; i < len(g_items); i++ {
 		fmt.Println(g_items[i])
 	}
 }
