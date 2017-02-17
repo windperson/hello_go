462c42a34bd2eb1e0522b816c5f7a3a90b770557
Charles Pao <windperson@gmail.com>
2017-02-17 16:40:05 +0800

    variadic function demo

--- functiondemo/main.go
+++ functiondemo/main.go
@@ -2,34 +2,60 @@ package main
 
 import "fmt"
 
-func printWow(times int){
-    for i:=0;i<times;i++{
-        fmt.Println("Wow")
-    }
+func printWow(times int) {
+	for i := 0; i < times; i++ {
+		fmt.Println("Wow")
+	}
 }
 
 func max(i int, j int) int {
-    if (i>j){
-        return i
-    }else {
-        return j
-    }
+	if i > j {
+		return i
+	} else {
+		return j
+	}
 }
 
-func maxWithPointer(i int, j int, k *int) int{
-    if(i>j){
-        *k = i
-    }else {
-        *k = j
-    }
-    fmt.Println("k inside func=",k)
-    return *k
+func getMax(numbers ...int) int {
+	if len(numbers) == 1 {
+		return numbers[0]
+	}
+
+	var max = numbers[0]
+	for _, value := range numbers[1:] {
+		if value > max {
+			max = value
+		}
+	}
+	return max
+}
+
+func maxWithPointer(i int, j int, k *int) int {
+	if i > j {
+		*k = i
+	} else {
+		*k = j
+	}
+	fmt.Println("k inside func=", k)
+	return *k
 }
 
 func main() {
-    var i,j = 100,15
-    var ret int
-    fmt.Println("&ret=",&ret)
-    // fmt.Println("max of ",i," and ",j," is ",max(i,j))
-    fmt.Println("max of ",i," and ",j," is ",maxWithPointer(i,j, &ret))    
+	var i, j = 100, 15
+	var ret int
+	fmt.Println("&ret=", &ret)
+	// fmt.Println("max of ",i," and ",j," is ",max(i,j))
+	fmt.Println("max of ", i, " and ", j, " is ", maxWithPointer(i, j, &ret))
+
+	var data0 = []int{0, 100, -100}
+	max := getMax(data0...)
+	fmt.Println("max of ", data0, " is:", max)
+
+    var data1 = []int{-100}
+    max = getMax(data1...)
+    fmt.Println("max of ", data1, " is:", max)
+
+    var data2 = []int{+100,-100}
+    max = getMax(data2...)
+    fmt.Println("max of ", data2, " is:", max)
 }
