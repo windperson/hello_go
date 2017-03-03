de04c56d0bde76a2ac41f82ef8c1656646b12876
Charles Pao <windperson@gmail.com>
2017-03-03 16:25:05 +0800

    add mongoDB demo

---
+++ mgoDemo/main.go
@@ -0,0 +1,53 @@
+package main
+
+/*
+NOTE:
+ example is from http://www.mrwaggel.be/post/golang-mgo-findandmodify-auto-increment-id/
+ to run this example, 
+ 1. you need to have a connectable mongoDB which has a "testMongoDemo" collection on localhost,
+ 
+ 2. run following command to first add data:
+ use testMongoDemo
+ db.persons.insert({"_id": "counterForPersons", "counterValue": 0})
+
+ 3. install mgo & bson package:
+ go get -u -v gopkg.in/mgo.v2
+ go get -u -v gopkg.in/mgo.v2/bson
+
+*/
+
+import (
+	"fmt"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const cName = "testMongoDemo"
+
+func main() {
+	session, _ := mgo.Dial("localhost")
+	defer session.Close()
+
+	//select the "sandboxed" collection in mongoDB
+	collection := session.DB(cName).C("persons")
+
+	//Create a bson.M interface to store result to
+	var result bson.M
+
+	//This is to tell what field and how to modify it
+	changeInDocument := mgo.Change{
+		Update:    bson.M{"$inc": bson.M{"counterValue": 1}},
+		ReturnNew: true,
+	}
+
+	//Find the document that keeps the counter value, and apply the modification
+	_, err := collection.Find(bson.M{"_id": "counterForPersons"}).Apply(changeInDocument, &result)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("new increment Value is", result["counterValue"])
+
+}
